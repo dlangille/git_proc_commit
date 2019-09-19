9815610abdee580cdb94e4429589ee006271d17c
Dan Langille <dan@langille.org>
2019-09-19 13:06:59 +0200

    Move to using the FreeBSD git repo

--- git-show-commit.sh
+++ git-show-commit.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 # the latest commit we have processed
-latest="ee38cccad8f76b807206165324e7bf771aa981dc"
+latest="3bd153c2494182bb89915e6fc9222288c154285f" # [NEW PORT]: devel/py-oci - Wed Sep 18 17:14:34 2019 +0000
 
-repo_dir="/Users/dan/src/acme.sh"
+# before running this script:
+# mkdir ~/src
+# cd ~/src
+# git clone git@github.com:freebsd/freebsd-ports.git
+
+repo_dir=~/src/freebsd-ports
 
 GIT="/usr/bin/git"
 
-cd $repo_dir
+cd "${repo_dir}"
 commits=`${GIT} rev-list ${latest}..HEAD`
 for commit in $commits
 do
