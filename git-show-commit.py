8366be0f8e68c11105d0d104564d65ccc1899aa4
Dan Langille <dan@langille.org>
2019-09-19 13:17:06 +0200

    Add git-show-commit.py

---
+++ git-show-commit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import git
+repo = git.Repo("~/src/freebsd-ports")
+commits = list(repo.iter_commits("master", max_count=9))
+dir(commits[0])
+print(commits[0].author)
+print(commits[0].message)
+print(commits[0].hexsha)
