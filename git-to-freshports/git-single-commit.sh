1daa1ee6a29243b81cfa71ea773a75d32648d0a5
Dan Langille <dan@langille.org>
2020-07-14 00:57:40 +0000

    Fix this script so it works

--- git-to-freshports/git-single-commit.sh
+++ git-to-freshports/git-single-commit.sh
@@ -5,7 +5,7 @@
 # An idea from https://github.com/sarcasticadmin
 
 # a single commit hash
-$commit_hash=$1
+commit_hash=$1
 
 if [ ! -f config.sh ]
 then
@@ -35,8 +35,8 @@ logfile "XML dir is $XML"
 
 cd ${REPODIR}
 
-logfile "${SCRIPTDIR}/git-to-freshports-xml.py --path ${FRESHPORTS_JAIL_BASE_DIR}${PORTSDIRBASE}PORTS-head-git --single-commit --commit ${commit_hash} --output ${XML}"
-         ${SCRIPTDIR}/git-to-freshports-xml.py --path ${FRESHPORTS_JAIL_BASE_DIR}${PORTSDIRBASE}PORTS-head-git --single-commit --commit ${commit_hash} --output ${XML}
+logfile "${SCRIPTDIR}/git-to-freshports-xml.py --path ${FRESHPORTS_JAIL_BASE_DIR}${PORTSDIRBASE}PORTS-head-git --single-commit ${commit_hash} --output ${XML}"
+         ${SCRIPTDIR}/git-to-freshports-xml.py --path ${FRESHPORTS_JAIL_BASE_DIR}${PORTSDIRBASE}PORTS-head-git --single-commit ${commit_hash} --output ${XML}
 
 ${LOGGER} -t ${LOGGERTAG} ending
 logfile "ending"
